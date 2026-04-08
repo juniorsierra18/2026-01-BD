a8e0b8f910fe82f1b564cd8e7e654be1b2fe10e2
JuniorSierraMorales <125913687+juniorsierra18@users.noreply.github.com>
2026-04-08 12:47:36 -0500

    Archivo con las respuestas (Jorge David Junior Sierra Morales)

---
+++ respuesta.sql
@@ -0,0 +1,48 @@
+SELECT p.productname
+FROM products p
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM employees e
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM orders o
+        JOIN orderdetails od ON o.orderid = od.orderid
+        WHERE od.productid = p.productid
+        AND o.employeeid = e.employeeid
+    )
+);
+SELECT c.companyname
+FROM customers c
+WHERE (
+    SELECT MAX(od.unitprice)
+    FROM orders o
+    JOIN orderdetails od ON o.orderid = od.orderid
+    WHERE o.customerid = c.customerid
+) < 50;
+SELECT e.title, e.firstname, e.lastname
+FROM employees e
+WHERE EXISTS (
+    SELECT 1
+    FROM orders o
+    JOIN orderdetails od ON o.orderid = od.orderid
+    JOIN products p ON od.productid = p.productid
+    WHERE o.employeeid = e.employeeid
+    AND p.productname IN ('Gravad Lax', 'Mishi Kobe Niku')
+);
+SELECT e.firstname, e.lastname, c.companyname
+FROM employees e
+JOIN customers c ON c.city = 'Bruxelles'
+WHERE (e.employeeid, c.customerid) IN (
+    SELECT o.employeeid, o.customerid
+    FROM orders o
+    JOIN shippers s ON o.shipvia = s.shipperid
+    WHERE s.companyname = 'Speedy Express'
+);
+SELECT e.firstname, e.lastname, e.address, e.city, e.region
+FROM employees e
+WHERE EXISTS (
+    SELECT 1
+    FROM orders o
+    WHERE o.employeeid = e.employeeid
+      AND o.shipcountry = 'Belgium'
+);
